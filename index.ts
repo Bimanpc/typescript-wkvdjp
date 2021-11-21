092020202bdf1ba9329191dd9c58232a3188f2e3
Bimanpc <greekfrezyness@hotmail.com>
2021-11-21 19:28:48 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript By Billman 2.0</h1>`;
